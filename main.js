cd9f94f452b567a0d9805b13ee5c0da27e2640e9
ShibamMaity <86520976+ShibamMaity@users.noreply.github.com>
2022-05-14 15:57:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-Webcam.set({
-    width:350,
-    height:300,
-    image_format : 'png',
-    png_quality:90
-});
+    Webcam.set({
+     width:350,
+     height:300,
+     image_format : 'png',
+     png_quality:90
+ });
 
-camera = document.getElementById("camera");
+ camera = document.getElementById("camera");
 
 Webcam.attach( '#camera' );
 
@@ -19,3 +19,23 @@ function take_snapshot()
 console.log('ml5 versiom:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wDJU2BVu4/model.json',modelLoaded);
+
+ function modelLoaded() {
+     console.log('Model Loaded!');
+ }
+
+ function check()
+ {
+     img = document.getElementById('captured_image');
+     classifier.classify(img, gotResult);
+ }
+
+ function gotResult(error, results) {
+     if (error) {
+     console.error(error);
+     } else {
+       console.log(results);
+       document.getElementById("result_object_name").innerHTML = results[0].label;
+       document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); 
+     }
+ }
